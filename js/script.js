e9e6bf5a00bb5852c6970780c45a3a540fe1b7e9
madfauzy <madfauzy28@gmail.com>
2021-07-08 00:49:55 +0700

    Update parallax effect

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Scroll Smooth
 $('.page-scroll').on('click', function(e){
 
     let tujuan = $(this).attr('href');
@@ -8,4 +9,36 @@ $('.page-scroll').on('click', function(e){
     }, 1000, 'swing')
 
     e.preventDefault();
+})
+
+// Parallax
+$(window).on('load', function(){
+    $('.pKiri').addClass('pShow');
+    $('.pKanan').addClass('pShow');
+})
+
+$(window).scroll(function(){
+    var wScroll = $(this).scrollTop();
+
+    // Jumbotron
+    $(".jumbotron img").css({
+        'transform' : 'translate(0px, '+ wScroll/4 + '%)'
+    })
+
+    $(".jumbotron h1").css({
+        'transform' : 'translate(0px, '+ wScroll/2 + '%)'
+    })
+
+    $(".jumbotron p").css({
+        'transform' : 'translate(0px, '+ wScroll/1.2 + '%)'
+    })
+
+    // Portfolio
+    if(wScroll > $('.portfolio').offset().top - 250){
+        $('.portfolio .thumbnail').each(function(i){
+            setTimeout(function(){
+                $('.portfolio .thumbnail').eq(i).addClass('show');
+            }, 500 * (i+1))
+        })
+    }
 })
